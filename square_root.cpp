10298e154459bf4edbe22f7d8a1d7765fda117fb
Boyko03 <boyko100@abv.bg>
2019-03-18 21:28:28 +0200

    Added square root program

---
+++ square_root.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+double square_root(double S, int n){
+    double x0, x1;
+    x0 = S;
+    //x((n+1)) = (x((n)) + S/x((n)))/2
+    x1 = (x0 + S/x0) / 2;
+    for(int i = 0; i < n; i++){
+        x1 = (x0 + S/x0) / 2;
+        x0 = x1;
+    }
+    return x1;
+}
+
+int main(){
+    printf("%f\n", square_root(100, 10));
+
+return 0;
+}
