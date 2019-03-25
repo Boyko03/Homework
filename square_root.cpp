2bce98a34e29f424486e9a436f2e13aa2a4ea526
Boyko03 <boyko100@abv.bg>
2019-03-25 18:40:03 +0200

    Modify square_root

--- square_root.cpp
+++ square_root.cpp
@@ -5,9 +5,10 @@ double square_root(double S, int n){
     x0 = S;
     //x((n+1)) = (x((n)) + S/x((n)))/2
     x1 = (x0 + S/x0) / 2;
-    for(int i = 0; i < n; i++){
-        x1 = (x0 + S/x0) / 2;
-        x0 = x1;
+    for(int i = 1; i < n; i++){
+        x1 = (x1 + S/x1) / 2;
+     //   x1 = (x0 + S/x0) / 2;
+     //   x0 = x1;
     }
     return x1;
 }
